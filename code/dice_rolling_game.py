72cbbdc9c9ac0bda0b3d4c711a4bb881450a62af
Trinh Mai Chi <trinhmaichi655@gmail.com>
2025-09-16 19:29:30 +0200

    improve dice rolling game

--- code/dice_rolling_game.py
+++ code/dice_rolling_game.py
@@ -1,20 +1,35 @@
 import random
-# Loop
-a = True 
-while a == True:
+total = 0
+# Loop  
+while True:
 # Ask: roll the dice?
     answer = input("Roll the dice? (y/n): ")
     # If user enter y
     if answer in ("y", "Y"):
-        #   Generate two random numbers
-        #   Print them
-        print("(",random.randint(1,6),", ",random.randint(1,6),")")
+        while True:
+            try:
+                number = int(input("How many dice?: "))
+                total += 1
+                if number <= 0:
+                    print("Please enter a valid number.")
+                    continue
+                break                
+            except ValueError:
+                print("Please enter a valid number.")
+        dice = ""
+        #   Generate random numbers 
+        for i in range(number):
+            each_dice = random.randint(1,6)
+            dice += str(each_dice) + ", "
+        dice = dice[:-2]
+        print(f'({dice})')
+        print("Number of times dice are rolled: ",total)
     # If user enter n
     elif answer in ("n", "N"):
         #   Print thank you message
         print("Thanks for playing!")
         #   Terminate
-        a = False
+        break
     # Else
     else: 
         #   Print invalide choice
